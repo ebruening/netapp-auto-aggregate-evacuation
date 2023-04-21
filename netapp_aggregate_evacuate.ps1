6809543d4f57e81d87b4010e30238476d130ff24
ebruening <39557298+ebruening@users.noreply.github.com>
2023-04-21 21:38:53 +0000

    Update netapp_aggregate_evacuate.ps1

--- netapp_aggregate_evacuate.ps1
+++ netapp_aggregate_evacuate.ps1
@@ -1,67 +1,71 @@
 # Import Modules
 IF (-not (Get-Module -Name DataOntap)) {
-        Import-Module DataOntap
-    }
+    Import-Module DataOntap
+}
 
-# Get-Credentials
-$netappcreds = Get-Credential -Message "Enter your NetApp Storage Credentials"
+#### var - start ####
+$cl = "yourcluster"
+$srcaggr = "yoursrcaggr"
+$destaggr = "yourdestaggr"
+#### var - end ####
 
 # Connect to storage systems
-Function connect-netapp
-    {
-    Connect-NcController 192.168.1.50 -credential $netappcreds
-    }
+Function connect-netapp {
+    #### connect to NetApp - start ####
+    $global:CurrentNcController = $null
 
-Function getvolmoves
-    {
-    # Get List of current volmoves
-    $global:currentvolmoves = Get-NcVolMove | Where {$_.State -eq "healthy"}
+    $NACL = $cl + ".yourdnssuffix"
+    if ($null -eq $nacred) {
+        Write-Output "Bitte die NetApp Anmeldedaten eingeben"
+        $nacred = Get-Credential
+    }
+    Connect-NcController -Name $NACL -HTTPS -Credential $nacred
+    #### connect to NetApp - end ####
+}
+
+Function getvolmoves {
+    # currently move volume list
+    $global:currentvolmoves = Get-NcVolMove | Where { $_.State -eq "healthy" }
     $global:counter = 0
 
-    ForEach ($volmove in $currentvolmoves)
-        {
+    ForEach ($volmove in $currentvolmoves) {
         $global:counter++
         Write-Host $volmove.Volume "is still moving to" $volmove.DestinationAggregate "- Percent Complete =" $volmove.PercentComplete -ForegroundColor Yellow
-        }
     }
+}
 
 # Connect to storage system
 connect-netapp
 
-# Aggregate to evacuate
-$evacaggr = "aggr1_CLUSTER96_01_data"
+#### moving vols to new aggr - start ####
 
-# Destination aggregate
-$destaggr = "aggr2_CLUSTER96_01_data"
+# vol's to move from $srcaggr, exclude audit vols
+$vollist = Get-NcVol -Aggregate $srcaggr | Where-Object { $_.Name -notlike "*MDV*" }
 
-# Get list of volumes on aggregate
-$listofvols = Get-NcVol | Where {$_.Aggregate -like $evacaggr -and $_.Name -notlike "*root*"}
-
-ForEach ($vol in $listofvols)
-    {
+ForEach ($vo in $vollist) {
+    Start-Transcript "D:\log\$NACL-$srcaggr-volmove$(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S").log"
 
     # Look for vol match in list of current vols
-    $volmovematch = Get-NcVolMove | Where {$_.Volume -eq $vol.Name}
+    # $volmovematch = Get-NcVolMove | Where { $_.Volume -eq $vo.Name }
     
     getvolmoves
 
-    IF ($global:counter -ge 4)
-        {
-        Do
-            {
-            $date = (Get-Date).Tostring("HH:mm")
-            Write-Host "$date - Vol move counter is greater than 4, sleeping 5 mins..." -ForegroundColor Yellow
+    IF ($global:counter -ge 4) {
+        Do {
+            Write-Host "$(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S") - Vol move counter is greater than 4, sleeping 5 mins..." -ForegroundColor Yellow
             sleep 300
             getvolmoves
-            }
-        Until ($global:counter -lt 4)
         }
+        Until ($global:counter -lt 4)
+    }
     
            
-    IF (!$volmovematch -and $global:counter -lt 4)
-            {
-            Write-Host "vol move counter is = " $global:counter
-            Write-Host $vol.name "is now moving" -ForegroundColor Green
-            Start-NcVolMove -DestinationAggregate $destaggr -Vserver $vol.Vserver -Name $vol.Name
-            }
+    IF ($global:counter -lt 4) {
+        Write-Host "$(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S") - currently runing volume move jobs = " $global:counter
+        Write-Host "$(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S") - " $vo.name "is now moving to" $destaggr -ForegroundColor Green
+        Start-NcVolMove -DestinationAggregate $destaggr -Vserver $vo.Vserver -Name $vo.Name | Out-Null
     }
+    Stop-Transcript
+}
+#Stop-Transcript
+#### moving vols to new aggr - end ####
